a0f371ddeab6fca504961843d88d1eba09e609af
Edwin Aloo <edwin.aloo@student.moringaschool.com>
2023-03-09 23:18:00 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+// for (let age = 30; age < 40; age++) {
+    // console.log(`I'm ${age} years old. Happy birthday to me!`);
+    // debugger;
+//   }
+ function writeCards(names, eventName){
+    let messages =[];
+    for(i =0;i<names.length;i++){
+        console.log(i);
+        console.log(messages.push(`Thank you, ${names},for the wonderful ${eventName}gift!`));
+
+    }
+    return messages;
+ } 
+ writeCards(["Ada", "Ali", "Brendan"], "birthday");
